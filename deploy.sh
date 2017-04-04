0a53be79b60ec099cb484753ed783de2f5ad1d90
Chris Spring <christopher.spring@ge.com>
2017-04-04 08:56:55 -0500

    created shell script to deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/bash -v
+if [ -z "$1" ]; then
+  echo "Provide target root folder as argument 1"
+  return 1
+fi
 
 TARGET_ROOT=$1
 sudo cp -rf config.txt $TARGET_ROOT/boot/config.txt
@@ -7,4 +11,6 @@ sudo cp ./usr/cmdline.txt $TARGET_ROOT/boot/
 sudo cp -rf ./99-fbturbo.conf $TARGET_ROOT/linux/usr/share/X11/xorg.conf.d/99-fbturbo.conf
 sudo mkdir $TARGET_ROOT/linux/etc/X11/xorg.conf.d
 sudo cp -rf ./usr/99-calibration.conf-5 $TARGET_ROOT/linux/etc/X11/xorg.conf.d/99-calibration.conf
-sudo reboot
+
+sudo cp usr/tft9341-overlay.dtb
+sudo cp usr/tft35a-overlay.dtb
