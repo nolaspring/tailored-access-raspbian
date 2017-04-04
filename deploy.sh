5cd77276ec1757c5618d6aa4e48b5e36b95ad830
Chris Spring <christopher.spring@ge.com>
2017-04-04 08:46:24 -0500

    added deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+TARGET_ROOT=$1
+sudo cp -rf config.txt $TARGET_ROOT/boot/config.txt
+sudo cp ./usr/cmdline.txt $TARGET_ROOT/boot/
+#depricated - sudo cp ./usr/inittab /etc/
+sudo cp -rf ./99-fbturbo.conf $TARGET_ROOT/linux/usr/share/X11/xorg.conf.d/99-fbturbo.conf
+sudo mkdir $TARGET_ROOT/linux/etc/X11/xorg.conf.d
+sudo cp -rf ./usr/99-calibration.conf-5 $TARGET_ROOT/linux/etc/X11/xorg.conf.d/99-calibration.conf
+sudo reboot
