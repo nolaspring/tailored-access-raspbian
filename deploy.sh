c790de76e9448ae45a677dbb01bc1845a9249f58
Chris Spring <christopher.spring@ge.com>
2017-04-04 19:30:08 -0500

    pulled off wifi configs

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,15 @@ if [ -z "$1" ]; then
   exit
 fi
 
-if [ -z "$2" && -z "$3"]; then
-  echo "Provide ssid and wpa passphrase as arguments 2 and 3"
-  echo "deploy.sh [TARGET_ROOT] [SSID] [WPA_PASSPHRASE]"
-  exit
-fi
+# if [ -z "$2" && -z "$3"]; then
+#   echo "Provide ssid and wpa passphrase as arguments 2 and 3"
+#   echo "deploy.sh [TARGET_ROOT] [SSID] [WPA_PASSPHRASE]"
+#   exit
+# fi
 
 TARGET_ROOT=$1
-SSID_0=$2
-WPA_PASSPHRASE=$3
+# SSID_0=$2
+# WPA_PASSPHRASE=$3
 
 sudo cp -rf config.txt $TARGET_ROOT/boot/config.txt
 sudo cp ./usr/cmdline.txt $TARGET_ROOT/boot/
@@ -22,7 +22,7 @@ sudo cp -rf ./99-fbturbo.conf $TARGET_ROOT/linux/usr/share/X11/xorg.conf.d/99-fb
 sudo mkdir $TARGET_ROOT/linux/etc/X11/xorg.conf.d
 sudo cp -rf ./usr/99-calibration.conf-5 $TARGET_ROOT/linux/etc/X11/xorg.conf.d/99-calibration.conf
 
-wpa_passphrase where_are_my_paaaaants the_casa_again >> /raspbian/linux/etc/wpa_supplicant/wpa_supplicant.conf
+# wpa_passphrase where_are_my_paaaaants the_casa_again >> /raspbian/linux/etc/wpa_supplicant/wpa_supplicant.conf
 sudo cp ./usr/tft9341-overlay.dtb /boot/overlays
 sudo cp ./usr/tft35a-overlay.dtb /boot/overlays
 
