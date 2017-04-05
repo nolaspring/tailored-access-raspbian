fe6cb6fb1c306ae31d57e5ffa9c9546092546d65
Chris Spring <christopher.spring@ge.com>
2017-04-04 22:33:24 -0500

    working touch-lcd, addjusted overscan

--- deploy.sh
+++ deploy.sh
@@ -16,17 +16,21 @@ DRIVER_ROOT=$2
 # SSID_0=$2
 # WPA_PASSPHRASE=$3
 
-sudo cp -rf config.txt $TARGET_ROOT/boot/config.txt
-sudo cp cmdline.txt $TARGET_ROOT/boot/
+sudo cp  ./config.txt $TARGET_ROOT/boot/config.txt
+sudo cp ./cmdline.txt $TARGET_ROOT/boot/
+sudo cp ./modules $TARGET_ROOT/etc/modules-load.d/
 #depricated - sudo cp ./usr/inittab /etc/
 sudo cp -rf ./99-fbturbo.conf $TARGET_ROOT/linux/usr/share/X11/xorg.conf.d/99-fbturbo.conf
 sudo rm -rf $TARGET_ROOT/linux/etc/X11/xorg.conf.d
 sudo mkdir $TARGET_ROOT/linux/etc/X11/xorg.conf.d
 sudo cp -rf ./99-calibration.conf $TARGET_ROOT/linux/etc/X11/xorg.conf.d/99-calibration.conf
 
-# wpa_passphrase where_are_my_paaaaants the_casa_again >> /raspbian/linux/etc/wpa_supplicant/wpa_supplicant.conf
+sudo wpa_passphrase where_are_my_paaaaants the_casa_again >> /raspbian/linux/etc/wpa_supplicant/wpa_supplicant.conf
+sudo wpa_passphrase where_are_my_pants the_casa_again >> /raspbian/linux/etc/wpa_supplicant/wpa_supplicant.conf
+
 sudo cp $DRIVER_ROOT/tft9341-overlay.dtb $TARGET_ROOT/boot/overlays
 sudo cp $DRIVER_ROOT/tft35a-overlay.dtb $TARGET_ROOT/boot/overlays
+
 
 
 exit
