2973b72290ed50a85016e56665b0c596ebc61a89
Chris Spring <christopher.spring@ge.com>
2017-04-04 19:37:00 -0500

    clean x11 xorg.d directory

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,10 @@ DRIVER_ROOT=$2
 # WPA_PASSPHRASE=$3
 
 sudo cp -rf config.txt $TARGET_ROOT/boot/config.txt
-sudo cp ./usr/cmdline.txt $TARGET_ROOT/boot/
+sudo cp cmdline.txt $TARGET_ROOT/boot/
 #depricated - sudo cp ./usr/inittab /etc/
 sudo cp -rf ./99-fbturbo.conf $TARGET_ROOT/linux/usr/share/X11/xorg.conf.d/99-fbturbo.conf
+sudo rm -rf $TARGET_ROOT/linux/etc/X11/xorg.conf.d
 sudo mkdir $TARGET_ROOT/linux/etc/X11/xorg.conf.d
 sudo cp -rf ./99-calibration.conf $TARGET_ROOT/linux/etc/X11/xorg.conf.d/99-calibration.conf
 
