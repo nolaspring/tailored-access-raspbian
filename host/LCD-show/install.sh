7166f08ad2f88699cb43e606e6fb8de0dd1d16fe
Chris Spring <christopher.spring@ge.com>
2017-04-21 15:43:26 -0500

    +x on the LCD script

--- host/LCD-show/install.sh
+++ host/LCD-show/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -v
 pushd .artifacts/LCD-show
-sudo ./LCD35-show 
+sudo chmod +x ./LCD35-show
+sudo ./LCD35-show
 echo '3.5 inch TFT configure, reboot needed for settings to take effect'
 popd
