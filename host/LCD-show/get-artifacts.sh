3737fdccac4c5bab0ceda3d27e12315bcde6c3fd
Chris Spring <christopher.spring@ge.com>
2017-04-21 13:43:44 -0500

    corrected relative path issues and such

--- host/LCD-show/get-artifacts.sh
+++ host/LCD-show/get-artifacts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -v
-
+mkdir .artifacts
+pushd .artifacts
 git clone https://github.com/nolaspring/LCD-show.git
+popd
--- host/wifi/configure.sh
+++ host/wifi/configure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -v
-cp /disk/linux/usr/wpa_supplicant.conf /disk/boot
-wpa_passphrase 'where_are_my_paaaaants' 'the_casa_again' >> /disk/boot/wpa_supplicant.conf
+cp /disks/linux/usr/wpa_supplicant.conf /disks/boot
+wpa_passphrase 'where_are_my_paaaaants' 'the_casa_again' >> /disks/boot/wpa_supplicant.conf
