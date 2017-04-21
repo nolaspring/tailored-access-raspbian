8af87b0f42703de2df309c4084577e66cbbffc90
Chris Spring <christopher.spring@ge.com>
2017-04-21 14:56:01 -0500

    checkout issue

--- host/LCD-show/get-artifacts.sh
+++ host/LCD-show/get-artifacts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pushd .artifacts
 git clone https://github.com/nolaspring/LCD-show.git
-git checkout tailored-access-raspbian
+git pull
+git checkout origin tailored-access-raspbian
 popd
