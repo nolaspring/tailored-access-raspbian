9416fddb836400826a7a5a73bf67a963216ba498
Chris Spring <christopher.spring@ge.com>
2017-04-21 15:46:23 -0500

    working wifi, added ssh

--- host/wifi/configure.sh
+++ host/wifi/configure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -v
 cp /disks/linux/etc/wpa_supplicant/wpa_supplicant.conf /disks/boot
 wpa_passphrase 'where_are_my_paaaaants' 'the_casa_again' >> /disks/boot/wpa_supplicant.conf
+sudo touch /disks/boot/ssh
